331abc47346a6707f05d66d26ef3d89cd28cbf99
Vitor Soares <vitorsoaresf@gmail.com>
2023-08-02 08:47:47 -0300

    feat: includes nodemon and add routes

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
 const http = require("http");
 const port = 8000;
+const routes = {
+  "/": "curso de node",
+  "/livros": "Entrei na página de livros",
+  "/autores": "Listagem de autores",
+  "/editora": "Listagem de editoras",
+};
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, { "Content-Type": "text/plain" });
-  res.end("Curso de node");
+  res.end(routes[req.url]);
 });
 
 server.listen(port, () => {
