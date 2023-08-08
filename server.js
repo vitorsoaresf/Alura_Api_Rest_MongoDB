ae6ba8e93b5b4433b3be459f29e138191c125b1c
Vitor Soares <vitorsoaresf@gmail.com>
2023-08-08 06:48:45 -0300

    feat: use express

--- server.js
+++ server.js
@@ -1,17 +1,6 @@
-const http = require("http");
-const port = 8000;
-const routes = {
-  "/": "curso de node",
-  "/livros": "Entrei na página de livros",
-  "/autores": "Listagem de autores",
-  "/editora": "Listagem de editoras",
-};
+import app from "./src/app.js";
+const port = process.env.PORT || 8000;
 
-const server = http.createServer((req, res) => {
-  res.writeHead(200, { "Content-Type": "text/plain" });
-  res.end(routes[req.url]);
-});
-
-server.listen(port, () => {
+app.listen(port, () => {
   console.log(`Servidor rodando na porta http://localhost:${port}`);
 });
