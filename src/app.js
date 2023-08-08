addbfc354f86b3c1fb1b05c4e69efa1e8c4fb342
Vitor Soares <vitorsoaresf@gmail.com>
2023-08-08 08:08:42 -0300

    feat: implements routes from list book, put and get by id

--- src/app.js
+++ src/app.js
@@ -16,10 +16,28 @@ app.get("/livros", (req, res) => {
   res.status(200).json(livros);
 });
 
+app.get("/livros/:id", (req, res) => {
+  let index = searchBook(req.params.id);
+  livros[index].titulo = req.body.titulo;
+  res.json(livros[index]);
+});
+
 app.post("/livros", (req, res) => {
   console.log(req.body);
   livros.push(req.body);
   res.status(201).send("Livro criado com sucesso!");
 });
+
+app.put("/livros/:id", (req, res) => {
+  let index = searchBook(req.params.id);
+  livros[index].titulo = req.body.titulo;
+  res.json(livros[index]);
+});
+
+const searchBook = (id) => {
+  let findIndex = 0;
+  findIndex = livros.findIndex((book) => book.id == id);
+  return findIndex;
+};
 
 export default app;
