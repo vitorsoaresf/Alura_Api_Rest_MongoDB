677f6b0aa06ccd40b9030af1497cfd92b19cf993
Vitor Soares <vitorsoaresf@gmail.com>
2024-06-04 15:56:41 -0300

    feat: create Livro model

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import express from "express";
 import { DB } from "./config/dbConnect.js";
+import routes from "./routes/index.js";
 
 DB.on("error", console.log.bind(console, "Erro de conexão"));
 DB.once("open", () => {
@@ -9,47 +10,35 @@ DB.once("open", () => {
 const app = express();
 app.use(express.json());
 
-const livros = [
-  { id: 1, titulo: "Senhor dos anéis" },
-  { id: 2, titulo: "O Hobbit" },
-];
+routes(app);
+// app.get("/livros/:id", (req, res) => {
+//   let index = searchBook(req.params.id);
+//   livros[index].titulo = req.body.titulo;
+//   res.json(livros[index]);
+// });
 
-app.get("/", (req, res) => {
-  res.status(200).send("Curso de Node");
-});
+// app.post("/livros", (req, res) => {
+//   console.log(req.body);
+//   livros.push(req.body);
+//   res.status(201).send("Livro criado com sucesso!");
+// });
 
-app.get("/livros", (req, res) => {
-  res.status(200).json(livros);
-});
+// app.put("/livros/:id", (req, res) => {
+//   let index = searchBook(req.params.id);
+//   livros[index].titulo = req.body.titulo;
+//   res.json(livros[index]);
+// });
 
-app.get("/livros/:id", (req, res) => {
-  let index = searchBook(req.params.id);
-  livros[index].titulo = req.body.titulo;
-  res.json(livros[index]);
-});
+// app.delete("/livros/:id", (req, res) => {
+//   let index = searchBook(req.params.id);
+//   livros.splice(index, 1);
+//   res.send("Excluido com sucesso!");
+// });
 
-app.post("/livros", (req, res) => {
-  console.log(req.body);
-  livros.push(req.body);
-  res.status(201).send("Livro criado com sucesso!");
-});
-
-app.put("/livros/:id", (req, res) => {
-  let index = searchBook(req.params.id);
-  livros[index].titulo = req.body.titulo;
-  res.json(livros[index]);
-});
-
-app.delete("/livros/:id", (req, res) => {
-  let index = searchBook(req.params.id);
-  livros.splice(index, 1);
-  res.send("Excluido com sucesso!");
-});
-
-const searchBook = (id) => {
-  let findIndex = 0;
-  findIndex = livros.findIndex((book) => book.id == id);
-  return findIndex;
-};
+// const searchBook = (id) => {
+//   let findIndex = 0;
+//   findIndex = livros.findIndex((book) => book.id == id);
+//   return findIndex;
+// };
 
 export default app;
--- src/config/dbConnect.js
+++ src/config/dbConnect.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 import { credentials } from "./env.js";
 
 mongoose.connect(
-  `mongodb+srv://${credentials.username}:${credentials.password}@cluster0.fwnizlg.mongodb.net/?retryWrites=true&w=majority&appName=alura-mongodb`
+  `mongodb+srv://${credentials.username}:${credentials.password}@cluster0.fwnizlg.mongodb.net/alura-node`
 );
 
 export const DB = mongoose.connection;
