df9b887a6a9081de3bc72c0bfc18d9313e52d167
Vitor Soares <vitorsoaresf@gmail.com>
2024-06-05 08:42:14 -0300

    feat: implements associate entity and search book query param

--- src/controllers/livroController.js
+++ src/controllers/livroController.js
@@ -2,25 +2,31 @@ import { livros } from "../models/Livro.js";
 
 class LivroController {
   static listarLivros = (req, res) => {
-    livros.find((err, livros) => {
-      res.status(200).json(livros);
-    });
+    livros
+      .find()
+      .populate("autor")
+      .exec((err, livros) => {
+        res.status(200).json(livros);
+      });
   };
 
   static listarLivroPorId = (req, res) => {
     const idBook = req.params.id;
 
-    livros.findById(idBook, (err, livros) => {
-      if (err) {
-        res
-          .status(400)
-          .send({ message: `${err.message} - falha ao carregar livro` });
-      } else {
-        res.status(200).send(livros);
-      }
+    livros
+      .findById(idBook)
+      .populate("autor", "nome")
+      .exec((err, livros) => {
+        if (err) {
+          res
+            .status(400)
+            .send({ message: `${err.message} - falha ao carregar livro` });
+        } else {
+          res.status(200).send(livros);
+        }
 
-      res.status(200).json(livros);
-    });
+        res.status(200).json(livros);
+      });
   };
 
   static cadastrarLivros = (req, res) => {
@@ -62,6 +68,14 @@ class LivroController {
       } else {
         res.status(204).send();
       }
+    });
+  };
+
+  static listarLivroPorEditora = (req, res) => {
+    const editora = req.query.editora;
+
+    livros.find({ editora: editora }, {}, (err, livros) => {
+      res.status(200).send(livros);
     });
   };
 }
